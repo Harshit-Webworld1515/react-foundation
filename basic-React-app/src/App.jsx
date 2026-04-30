afc951ae582c72147d039e11bf2fcd96e3cf480d
Harshit <harshitpandit3525@gmail.com>
2026-04-30 20:06:44 +0530

    setup and first component done

---
+++ basic-React-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+// import means bring this component from other files
+import Title from './Title'
+import { Description } from './Title'
+function App() {
+
+  return (
+    <div className="App">
+      <Title />
+      <p>This is a simple React app.</p>
+      <Description />
+    </div>
+  )
+}
+
+export default App
---
+++ basic-React-app/src/Title.jsx
@@ -0,0 +1,10 @@
+function Title() {
+  return <h1>Hello, React!</h1>
+}
+function Description() {
+  return <p>This is a description of the react app component.</p>
+}
+
+// export means send this component to other files
+export default Title
+export { Description }
