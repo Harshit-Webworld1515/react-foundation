8a4a8ff2ef28f91ebbc12a3e4718d63df0e74359
Harshit <harshitpandit3525@gmail.com>
2026-05-01 06:58:18 +0530

    Add JSX expression rendering and include ProductTab component in App

--- basic-React-app/src/App.jsx
+++ basic-React-app/src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 // import means bring this component from other files
 import Title from './Title'
 import { Description } from './Title'
+// import Product from './Product'
+import ProductTab from './ProductTab'
 function App() {
 
   return (
@@ -10,6 +12,9 @@ function App() {
       <Title />
       <p>This is a simple React app.</p>
       <Description />
+      <p>Calculating : 17*167={17 * 167}</p>
+      <br />
+      <ProductTab />
     </>
     // </div>
   )
