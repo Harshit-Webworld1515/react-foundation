aadd4172979b0b3c48dd9295c6c5faf8bb8d5d7c
Harshit <harshitpandit3525@gmail.com>
2026-05-01 06:39:11 +0530

    Add ProductTab component rendering multiple Product components

---
+++ basic-React-app/src/ProductTab.jsx
@@ -0,0 +1,13 @@
+import Product from "./Product"
+function ProductTab() {
+    return (
+        <>
+            <h1>Product Tab</h1>
+            <Product />
+            <Product />
+            <Product />
+        </>
+    )
+}
+
+export default ProductTab
