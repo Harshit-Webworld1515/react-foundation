a51a565944513bac8569ff79e69952923e939960
Harshit <harshitpandit3525@gmail.com>
2026-05-01 07:00:44 +0530

    Add JSX expression in Description component and style Product component

--- basic-React-app/src/Title.jsx
+++ basic-React-app/src/Title.jsx
@@ -2,7 +2,9 @@ function Title() {
   return <h1>Hello, React!</h1>
 }
 function Description() {
-  return <p>This is a description of the react app component.</p>
+  let para="paragraph1"
+  // curly braces means we can write pure javascript code inside jsx
+  return <p>{para.toUpperCase()}: this is a description of the react app component.</p>
 }
 
 // export means send this component to other files
