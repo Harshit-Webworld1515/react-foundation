e317f2f9180edf496583d92199ef3fb4dbbb0104
Harshit <harshitpandit3525@gmail.com>
2026-05-01 06:37:14 +0530

    Add Product component and apply basic styling

---
+++ basic-React-app/src/Product.jsx
@@ -0,0 +1,11 @@
+import './Product.css'
+function Product() {
+    return (
+        <div className="Product">
+            <h3>Product Component</h3>
+            <h5>This is a product component.</h5>
+        </div>  
+    )
+}
+
+export default Product
