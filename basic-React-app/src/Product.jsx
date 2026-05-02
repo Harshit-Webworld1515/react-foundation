7179b5375bc2d232f41a29936056eb7317edbd1c
Harshit <harshitpandit3525@gmail.com>
2026-05-02 11:31:21 +0530

    feat: add Item and Article components showcasing React props usage

--- basic-React-app/src/Product.jsx
+++ basic-React-app/src/Product.jsx
@@ -1,11 +1,34 @@
 import './Product.css'
+import Button from './Button';
 function Product() {
     return (
         <div className="Product">
             <h3>Product Component</h3>
             <h5>This is a product component.</h5>
-        </div>  
+        </div>
     )
 }
-
-export default Product
+//Props are used to pass data from one component to another component.
+// Props are used to pass data from parent component to child component.
+function Item({ name, price=40 }) {
+    console.log(name, price)
+    return (
+        <div className="Item">
+            <h4>{name}</h4>
+            <p>Price: ${price/2}</p>
+        </div>
+    )
+}
+function Article({ para1 }) {
+    return (
+        <div className="Article">
+            <h3>Article Heading</h3>
+            <p>{para1}</p>
+            <Button text="like" />
+            <Button text="dislike" />
+        </div>
+    )
+}
+// export { Product, Item }
+export default Product;
+export { Item, Article };
