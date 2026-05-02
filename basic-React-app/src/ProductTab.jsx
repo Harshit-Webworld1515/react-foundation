c8056e172f1d856abad5dabd0fd6a6688f6f92c8
Harshit <harshitpandit3525@gmail.com>
2026-05-02 11:39:49 +0530

    Created  Itemlist, and ArticleTab components with props usage and remove default vite styling of app.css

--- basic-React-app/src/ProductTab.jsx
+++ basic-React-app/src/ProductTab.jsx
@@ -1,4 +1,4 @@
-import Product from "./Product"
+import Product, { Item , Article} from "./Product"
 function ProductTab() {
     return (
         <>
@@ -9,5 +9,29 @@ function ProductTab() {
         </>
     )
 }
+function Itemlist() {
+    let name1 = "Groceries"
+    let name2 = "gadget accessories"
+    let name3 = " clothing items"
+    let price = 100
+    return (
+        <div>
+            <Item name={name1} price={price} />
+            <Item name={name2} price={120} />
+            <Item name={name3} />
+        </div>
+    )
+}
+function ArticleTab() {
+    let para1 = "Sources in the Bangladesh Nationalist Party (BNP) said that Mr. Sarma’s remarks indicate that there is a segment in India that does not want restoration of ties. “We follow a ‘Bangladesh first’ foreign policy which is an official foreign policy of the BNP government and such comments that undermine our sovereignty will not be tolerated,” said a source in Dhaka."
+    let para2 = "The BNP has been in power for three terms since 1991 and has been in opposition for two terms. The party is currently in opposition and is facing a political crisis after the arrest of its leader, Khaleda Zia, on corruption charges. The party has been demanding the release of its leader and has been protesting against the government."
+    return (
+        <div>
+            <Article para1={para1} />
+            <Article para1={para2}  />
+        </div>
+    )
+}
 
 export default ProductTab
+export { Itemlist, ArticleTab }
