5bc6e6740aefc8b02d75dc5ea1a30269e331a566
Harshit <harshitpandit3525@gmail.com>
2026-05-03 23:50:02 +0530

    feat: add quality prop to Item and enhance Article with genre and conditional writer rendering

--- basic-React-app/src/Product.jsx
+++ basic-React-app/src/Product.jsx
@@ -10,20 +10,23 @@ function Product() {
 }
 //Props are used to pass data from one component to another component.
 // Props are used to pass data from parent component to child component.
-function Item({ name, price=40 }) {
-    console.log(name, price)
+function Item({ name, price=40, quality }) {
+    console.log(name, price, quality)
     return (
         <div className="Item">
             <h4>{name}</h4>
             <p>Price: ${price/2}</p>
+            <p>Quality: {quality}</p>
         </div>
     )
 }
-function Article({ para1 }) {
+function Article({ para1, genre ,writer }) {
     return (
         <div className="Article">
             <h3>Article Heading</h3>
             <p>{para1}</p>
+            <p>Genre: {genre}</p>
+            <p>Writer: {writer.a || writer.b}</p>
             <Button text="like" />
             <Button text="dislike" />
         </div>
