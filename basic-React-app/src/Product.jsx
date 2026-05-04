db977e0e49cfd7b155f4f4f5499f8eea300662ef
Harshit <harshitpandit3525@gmail.com>
2026-05-04 23:18:34 +0530

    Add conditional statement via ternary as well as if else ans show array in well list format

--- basic-React-app/src/Product.jsx
+++ basic-React-app/src/Product.jsx
@@ -8,25 +8,45 @@ function Product() {
         </div>
     )
 }
+
 //Props are used to pass data from one component to another component.
 // Props are used to pass data from parent component to child component.
+
 function Item({ name, price=40, quality }) {
     console.log(name, price, quality)
-    return (
-        <div className="Item">
+    // 1st way of using conditional rendering in React
+    if (quality=="bad") {
+        return (
+            <div className="Item">
             <h4>{name}</h4>
-            <p>Price: ${price/2}</p>
+            <h6>Special Discount!</h6>
+            <p>Price with discount: ${price/2}</p>
             <p>Quality: {quality}</p>
         </div>
     )
+}else{
+    return (
+        <div className="Item">
+        <h4>{name}</h4>
+        <h6> Limited offer!</h6>
+        <p>Price: ${price}</p>
+        <p>Quality: {quality}</p>
+    </div>
+)
 }
-function Article({ para1, genre ,writer }) {
+}
+function Article({ para1, genre ,writer,Liked }) {
+    const listItems = genre.map((item) => <li>{item}</li>);
     return (
         <div className="Article">
             <h3>Article Heading</h3>
+            <p>Genre: <ul>{listItems}</ul></p>
             <p>{para1}</p>
-            <p>Genre: {genre}</p>
             <p>Writer: {writer.a || writer.b}</p>
+            <p>Liked: {Liked}</p>
+            {/* Apply conditional rendering */}
+
+            {Liked > 70 ? <p>"Trending"</p>: <a href="#">Read more</a>}
             <Button text="like" />
             <Button text="dislike" />
         </div>
