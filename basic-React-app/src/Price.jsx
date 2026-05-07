2a3baf6ee3e552e3df8cc380e9986298b6f67f3d
Harshit <harshitpandit3525@gmail.com>
2026-05-07 23:46:06 +0530

    style: add styled Price component with flex layout

---
+++ basic-React-app/src/Price.jsx
@@ -0,0 +1,32 @@
+function Price({ OldPrice, NewPrice }) {
+    // let discount = ((OldPrice - NewPrice) / OldPrice) * 100
+    const Oldstyles = {
+        textDecoration: "line-through",
+        color: "red",
+    }
+    const Newstyles = {
+        color: "green",
+        fontWeight: "bold",
+
+    }
+    let styles = {
+        display: "flex",
+        gap: "10px",
+        alignItems: "center",
+        backgroundColor: "#ffe796",
+        borderRadius: "5px",
+        width: "300px",
+        height: "90px",
+        borderBottomRightRadius: "25px",
+        borderBottomLeftRadius: "25px",
+    }
+    return (
+         <div className="Price" style={styles}>
+            <span style={Oldstyles}>Old Price: ${OldPrice.toFixed(2)}</span>
+                &nbsp;&nbsp;
+            <span style={Newstyles}>New Price: ${NewPrice.toFixed(2)}</span>
+              
+        </div>
+    )
+}
+export default Price;
