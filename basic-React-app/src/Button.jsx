a11bdbc9441c10f17963192a6df139ae30be0405
Harshit <harshitpandit3525@gmail.com>
2026-05-09 23:12:03 +0530

    Add Button2 component with click, hover and double click events with event handler

--- basic-React-app/src/Button.jsx
+++ basic-React-app/src/Button.jsx
@@ -2,8 +2,28 @@ import './Product.css'
 function Button({ text }) {
     return (
         <>
-        <button>{text}</button>
+        <button className="button1">{text}</button>
         </>
     );
 }
+function Button2( ) {
+    return (
+        <div className="button2">
+        <button onClick={handleClick}>Click me</button>
+        <p onMouseOver={handleMouseOver}>Ayodhya Naresh Siyavar Ramchandra ki jai🙌🦾💪✊</p>
+        <button onDoubleClick={handleDoubleClick}>Double Click</button>
+        </div>
+    );
+
+}
+function handleClick( ) {
+    console.log("Hmre Raja Ram ji ki jai");
+}
+function handleMouseOver( ) {
+    console.log("Jai Shri Ram");
+}
+function handleDoubleClick( ) {
+    console.log("Double clicked!");
+}
 export default Button;
+export { Button2 };
