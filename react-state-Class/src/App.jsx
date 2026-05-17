7173e1b5b9f1481a981c54f8224d0b1b89b4a43c
Harshit <harshitpandit3525@gmail.com>
2026-05-17 23:58:10 +0530

    add LikeBtn component call in App.jsx and comments

--- react-state-Class/src/App.jsx
+++ react-state-Class/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Counter from './Counter.jsx'
+import LikeBtn from './LikeBtn.jsx'
 
 function App() {
   return (
  <>
  <h1>States in React</h1>
  <Counter />
+ <LikeBtn />
  </>
   )
 }
--- react-state-Class/src/Counter.jsx
+++ react-state-Class/src/Counter.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-export default function Counter() { 
+export default function Counter() {
     // let count = 0
     // this function not re-render the component, so the count willn't
     // update in the UI now using useState hook to update the count and re-render the component
@@ -8,17 +8,23 @@ export default function Counter() {
     //     count++
     //     console.log(count)
     // }
+
+    // this is the useState hook that will return an array with two elements
+    //state declaration and initialization
     const [count, setCount] = useState(0)
     // setCount is a updater function that will update the count value and trigger re-rendering of the component
     function handleClick() {
-    // setCount(count + 1) is an asynchronous function, it will update the count value and re-render the component 
+        // setCount(count + 1) is an asynchronous function, it will update the count value and re-render the component 
         setCount(count + 1)
         console.log(count) // this will log the old count value because setCount is asynchronous, it will update the count value after the current function execution is completed
     }
-    return(
+    return (
         <div className="Counter">
             <h2>No.of clicks: {count}</h2>
             <button onClick={handleClick}>count click</button>
         </div>
     )
 }
+// count is a state variable that holds the current count value,
+// and setCount is a function that updates the count value and triggers a re-render of the component.
+// When the button is clicked, handleClick is called, which updates the count value using setCount, and then re-renders the component.
