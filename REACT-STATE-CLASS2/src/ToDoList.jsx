9bac0f88c0ca79897da815cc11af181c530ff40b
Harshit <harshitpandit3525@gmail.com>
2026-07-05 13:46:02 +0530

    adding delete feature in ToDo app with help of filter

--- REACT-STATE-CLASS2/src/ToDoList.jsx
+++ REACT-STATE-CLASS2/src/ToDoList.jsx
@@ -9,13 +9,22 @@ export default function ToDoList() {
         setTasks((prevTasks) => ([...prevTasks, inputValue]));
         setInputValue('');
     }
+    //(_, i)likh diya.Matlab _  = item of array (ignore kar do) i = index Ye underscore sirf convention hai. Iska koi special meaning JavaScript me nahi hai.
+    const deleteTask = (index) => {
+        setTasks((prevTasks) => prevTasks.filter((_, i) => i !== index));
+    };
     return (
         <div>
             <h1>To Do List</h1>
             <input type="text" value={inputValue} onChange={addTask} placeholder="Enter a new task..." />
             <ul>
                 {tasks.map((task, index) => (
-                    <li key={index}>{task}</li>
+                    <li key={index}><span>{task}</span>
+                        &nbsp; &nbsp;
+                        <button key={index} onClick={() => deleteTask(index)}>
+                            Delete
+                        </button>
+                    </li>
                 ))}
             </ul>
             <button onClick={newTask}>
