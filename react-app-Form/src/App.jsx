964c7e59c96663682dc973f4d377b4aa8884a3d0
Harshit <harshitpandit3525@gmail.com>
2026-07-13 17:05:32 +0530

    Import Form component to app.jsx component

---
+++ react-app-Form/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Form from './Form'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Form</h1>
+      <p>An input form element whose value is controlled by React in this way is called a "controlled component". .</p>
+      <Form />
+    </div>
+  )
+}
+
+export default App
