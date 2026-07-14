7e7261ec55d96116af84d477dfc48f0ed73c75f8
Harshit <harshitpandit3525@gmail.com>
2026-07-14 18:31:05 +0530

    Handling Multiple input

--- react-app-Form/src/Form.jsx
+++ react-app-Form/src/Form.jsx
@@ -1,19 +1,43 @@
 import { useState } from 'react'
-import './App.css'
+import './Form.css'
 export default function Form() {
-  const [inpValue, setinpValue] = useState('')
+  // const [inpValue, setinpValue] = useState('')
+  const [inpValue, setinpValue] = useState(
+    {
+      user: "",
+      email: ""
+    }
+  )
   const handleinpValueChange = (e) => {
-    setinpValue(e.target.value)
+    // setinpValue(e.target.value)
+    setinpValue((prevValue) => ({
+      ...prevValue,
+      [e.target.name]: e.target.value
+    }))
   }
-//An input form element whose value is controlled by React in this way is called a "controlled component".
+  const handleSubmission = (e) => {
+    e.preventDefault();
+
+    console.log(`Name: ${inpValue.user}, Email: ${inpValue.email}`),
+    setinpValue((prevValue) => ({
+      ...prevValue,
+      user: "",
+      email: ""
+    }))
+  }
+  //An input form element whose value is controlled by React in this way is called a "controlled component".
   return (
     <>
-      <form action="">
-        <h2>My Form</h2>
-        <label htmlFor="Name">Name: </label>
-        <input type="text" id="Name" placeholder='Enter your name' onChange={handleinpValueChange} value={inpValue} />
+      <form onSubmit={handleSubmission} className="form">
+        <h2 className="h2">My Form</h2>
+        <label htmlFor="Name" className="label">Name: </label>
+        <input type="text" id="Name" placeholder='Enter your name' onChange={handleinpValueChange} value={inpValue.user} name='user' className="input" />
         <br />
-        <button type="submit">Submit</button>
+        {/* //handling Multiple Inputs */}
+        <label htmlFor="Email" className="label">Email: </label>
+        <input type="email" id="Email" placeholder='Enter your email' onChange={handleinpValueChange} value={inpValue.email} name='email' className="input" />
+        <br />
+        <button type="submit" className="button">Submit</button>
       </form>
     </>
   )
