5dce049b027b555ba6661177c09a0345631b4484
Harshit <harshitpandit3525@gmail.com>
2026-07-22 10:46:33 +0530

    feat: render WeatherApp as the root application component

---
+++ Material_UI/SkyCast-A Mini-Project/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Foundation from './Foundation'
+// import Searchbox from './SearchBox' 
+// import InfoBox from './InfoBox'
+import WeatherApp from './WeatherApp'
+function App() {
+  return (
+    <>
+      {/* <Foundation/> */}
+      {/* <Searchbox/> */}
+      {/* <InfoBox/> */}
+      <WeatherApp/>
+
+    </>
+  )
+}
+
+export default App
