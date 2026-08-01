573e686a40a8e858d1eaa4c924c511b5ae0bb3de
Harshit <harshitpandit3525@gmail.com>
2026-08-01 16:52:25 +0530

    feat: add todo slice with add, delete, and complete reducers

---
+++ Redux-Class/src/features/todo/todoSlice.js
@@ -0,0 +1,34 @@
+//Reducer
+import { createSlice, nanoid } from '@reduxjs/toolkit'
+const initialState = {
+    todos: [{ id: "abc", task: "Redux Learning", isDone: false }],
+}
+export const todoSlice = createSlice({
+    name: 'todo',
+    initialState,
+    reducers: {
+        addTodo: (state, action) => {
+            const newTodo = {
+                id: nanoid(),
+                task: action.payload,
+                isDone: false
+            }
+            state.todos.push(newTodo);
+        },
+        deleteTodo: (state, action) => {
+            state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+        },
+        markedAsDone: (state, action) => {
+            state.todos = state.todos.map((todo) => {
+                if (todo.id === action.payload) {
+                    todo.isDone = true;
+                }
+                return todo;
+            }
+            )
+        }
+    }
+})
+export const { addTodo, deleteTodo, markedAsDone } = todoSlice.actions;
+export default todoSlice.reducer;
+//Redux toolkit gives power of direct Mutation
