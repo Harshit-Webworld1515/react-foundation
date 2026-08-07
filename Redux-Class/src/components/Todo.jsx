150a6827df6457dac4871b6f85c4a8be4709fbe1
Harshit <harshitpandit3525@gmail.com>
2026-08-07 09:36:42 +0530

    feat:adding delete feature to our Todo designed by redux

--- Redux-Class/src/components/Todo.jsx
+++ Redux-Class/src/components/Todo.jsx
@@ -1,9 +1,17 @@
 import { useSelector } from 'react-redux'
 import AddForm from './AddForm'
+import { useDispatch } from 'react-redux'
+import { deleteTodo } from '../features/todo/todoSlice';
+
 
 export default function Todo() {
     const todos = useSelector((state) => state.todos);
+    const dispatch = useDispatch();
     console.log(todos);
+
+    const deleteList = (todo) => {
+        dispatch(deleteTodo(todo.id));
+    };
     return (
         <>
             <h1>Ready for Redux Todo</h1>
@@ -12,6 +20,7 @@ export default function Todo() {
                 {todos.map((todo) => (
                     <li key={todo.id}>
                         {todo.task} - {todo.isDone ? "Done" : "Not Done"}
+                        &nbsp; &nbsp; &nbsp; <button onClick={()=> deleteList(todo)} >Delete</button>
                     </li>
                 ))}
             </ul>
